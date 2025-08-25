518a19cc5d87c209a41b4e2918267b2a00b40ee1
Gogulancode <freshiaa@gmail.com>
2025-08-25 22:39:58 +0530

    Remove RouterOutlet Import

--- lt-india-erp/lt-india-erp/src/app/app.ts
+++ lt-india-erp/lt-india-erp/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-root',
   imports: [CommonModule],
-  templateUrl: './app.html',
+  imports: [CommonModule],
   styleUrl: './app.css'
 })
 export class App {
